9ec32de9bae7a69fa0ee7102a3353a6fcabde459
matheusminatto <matheusminatto1@gmail.com>
2015-05-06 17:25:00 -0300

    Revert a2fd4fd..2ec756d
    This rolls back to commit a2fd4fda17187d9e349b7f7ba4dc59ce21532d6c.

--- Assets/Scripts/BackNo.cs
+++ Assets/Scripts/BackNo.cs
@@ -1,29 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BackNo : MonoBehaviour
-{
+public class BackNo : MonoBehaviour {
 	public Sprite Hover, NotHover;
 	public GameObject PopUp;
 	public AudioClip HoverSound;
 
-	void OnMouseEnter ()
-	{
-		GetComponent<AudioSource> ().PlayOneShot (HoverSound, 1f);
-		gameObject.GetComponent<SpriteRenderer> ().sprite = Hover;
+	void OnMouseEnter(){
+		GetComponent<AudioSource>().PlayOneShot(HoverSound, 1f);
+		gameObject.GetComponent<SpriteRenderer>().sprite = Hover;
 	}
-	void OnMouseExit ()
-	{
-		gameObject.GetComponent<SpriteRenderer> ().sprite = NotHover;
+	void OnMouseExit(){
+		gameObject.GetComponent<SpriteRenderer>().sprite = NotHover;
 	}
-	void OnMouseDown ()
-	{
-		PopUp.SetActive (false);
-		Invoke ("Resume", 0.05f);
-		gameObject.GetComponent<SpriteRenderer> ().sprite = NotHover;
+	void OnMouseDown(){
+		PopUp.SetActive(false);
+		Invoke("Resume", 0.05f);
+		gameObject.GetComponent<SpriteRenderer>().sprite = NotHover;
 	}
-	void Resume ()
-	{
-		GameController.playing = true;		
+	void Resume(){
+		FindObjectOfType<GameController> ().playing = true;		
 	}
 }
